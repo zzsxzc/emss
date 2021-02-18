96553511e7fef038144c7590ff2d87b7b5fe34b6
zzsxzc <1350111595@qq.com>
2021-02-18 15:58:35 +0800

    Initial commit

---
+++ src/main/java/com/freedommxc/top/emss/emss/EmssApplication.java
@@ -0,0 +1,13 @@
+package com.freedommxc.top.emss.emss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmssApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/freedommxc/top/emss/emss/EmssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freedommxc.top.emss.emss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
